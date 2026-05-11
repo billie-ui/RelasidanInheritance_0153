1dc1947551a2a730d01e3736635e5d22bdb8b5d7
Billie Fauzan <kuliahkuliahbillie@gmail.com>
2026-05-11 15:07:06 +0700

    Menambahkan relasi ibu dan anak pada main

--- MainAgregasi.cpp
+++ MainAgregasi.cpp
@@ -3,3 +3,29 @@ using namespace std;
 
 #include "anak.h"
 #include "ibu.h"
+
+int main() {
+    ibu* varIbu = new ibu ("Dini");
+    ibu* varIbu2 = new ibu ("Novi");
+    anak* varAnak1 = new anak ("Tono");
+    anak* varAnak2 = new anak ("Rini");
+    anak* varAnak3 = new anak ("Dewi");
+
+    varIbu -> tambahAnak(varAnak1);
+    varIbu -> tambahAnak(varAnak2);
+    varibu2 -> tambahAnak(varAnak3);
+    varibu2 -> tambahAnak(varAnak1);
+
+    varIbu ->cetakAnak();
+    varIbu ->cetakAnak();
+
+    delete varIbu;
+    delete varibu2;
+    delete varAnak1;
+    delete varAnak2;
+    delete varAnak3;
+
+    system("pause");
+
+    return 0;
+}
