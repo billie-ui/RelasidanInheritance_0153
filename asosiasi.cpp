ea3ad0fee46f63fc990aa4939043e989a2aceb5e
Billie Fauzan <kuliahkuliahbillie@gmail.com>
2026-05-11 15:36:13 +0700

    `Menambahkan fitur tambah dan cetak daftar pasien pada class dokter`

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -52,3 +52,17 @@ void pasien::cetakdokter() {
     cout << endl;
 }
 
+void dokter::tambahPasien(pasien* pPasien) {
+    daftar_pasien.push_back(pPasien);
+    pPasien->tambahDokter(this);
+}
+
+void dokter::cetakPasien() {
+    cout << "Daftar Pasien dari dokter \"" << this->nama << "\":\n";
+    
+    for (auto& a : daftar_pasien) {
+        cout << a->nama << "\n";
+    }
+
+    cout << endl;
+}
